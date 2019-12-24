8c0d5f4dda6f35f622c6f737ed78148d9a4a19f2
manish <manish.tiwari@infostretch.com>
2019-12-24 23:18:57 +0530

    commit change

---
+++ src1/com/automationpoint/AppTest1.java
@@ -0,0 +1,17 @@
+package com.automationpoint;
+
+import org.testng.annotations.Test;
+
+public class AppTest1 {
+	 @Test()
+	    public void AppTest12( ) {
+	        System.out.println("working with maven project AppTest12");
+	    }
+
+	    @Test()
+	    public void AppTest13( ) {
+	        System.out.println("working with maven project AppTest13");
+	    }
+	   
+
+}

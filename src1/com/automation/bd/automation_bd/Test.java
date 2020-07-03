d7cb6a9fd76fc529f331f16fce9f2d9bc0a30e5d
matiwari-apttus <matiwari@apttus.com>
2020-07-03 19:18:24 +0530

    coomit change

---
+++ src1/com/automation/bd/automation_bd/Test.java
@@ -0,0 +1,10 @@
+package com.automation.bd.automation_bd;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world testing is going smooth");
+
+	}
+
+}
